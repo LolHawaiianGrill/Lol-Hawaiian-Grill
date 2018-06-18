9c05210afb267a7112946de341041ed914027b0b
Ashleigh <ashleigh.schuldt@gmail.com>
2018-06-18 16:16:24 -0600

    adding text on About page

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,5 +1,17 @@
 import * as React from 'react';
 
-export const Location = () => {
-    
+export default class Location extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return <div>
+            <h1>Locations</h1>
+            <h6>Sandy, Utah</h6>
+            <p> 
+                9460 S Union Sqare #100 <br></br>
+                Sandy, UT 84070
+            </p>
+                </div>
+    }
 }
