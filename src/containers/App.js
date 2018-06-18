d0964914290db542600b2af854c8177bdbd4cd36
Abigail Thelin <abbyjthelin@gmail.com>
2018-06-18 09:34:23 -0600

    folders and dependencies

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        Lol Hawaiian grill
+      </div>
+    );
+  }
+}
+
+export default App;
