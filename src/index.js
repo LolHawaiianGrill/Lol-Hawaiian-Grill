fddfe9cfefa437c961df749e15e8504eb5c6b991
Abigail Thelin <abbyjthelin@gmail.com>
2018-06-18 12:13:00 -0600

    added Browser Router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-
+    <BrowserRouter>
         <App />
-
+    </BrowserRouter>
 , document.getElementById('root'));
 registerServiceWorker();
