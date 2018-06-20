55c4a256f3fd38111005275b958d3d9905a5904f
Abigail Thelin <abbyjthelin@gmail.com>
2018-06-20 12:43:20 -0600

    html finished, styling left

---
+++ src/constants/home.jsx
@@ -0,0 +1,6 @@
+export class Messages{
+    static satisfy = 'Satisfy your craving for authentic island grindz at LOL Hawaiian Grill';
+    static polyPlates = 'Try our famous Poly Plates Daily special Thursday - Saturday';
+    static family = '"They treat you like family the moment you walk in" -Jessica F.';
+    static crowd = `Have a crowd to feed? <br/> Cater with LOL`;
+}
--- src/utilities/router.js
+++ src/utilities/router.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Home from '../components/Home';
+import { Home } from '../components/Home';
 import { About } from '../components/About';
 import { Location } from '../components/Location';
 
