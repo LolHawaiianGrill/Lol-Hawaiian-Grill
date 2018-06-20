bafecc87a666578c103e550d002d36bf10a0e6f1
Abigail Thelin <abbyjthelin@gmail.com>
2018-06-20 14:55:55 -0600

    styling done

--- src/constants/home.jsx
+++ src/constants/home.jsx
@@ -2,5 +2,5 @@ export class Messages{
     static satisfy = 'Satisfy your craving for authentic island grindz at LOL Hawaiian Grill';
     static polyPlates = 'Try our famous Poly Plates Daily special Thursday - Saturday';
     static family = '"They treat you like family the moment you walk in" -Jessica F.';
-    static crowd = `Have a crowd to feed? <br/> Cater with LOL`;
+    static crowd = `Have a crowd to feed? Cater with LOL`;
 }
