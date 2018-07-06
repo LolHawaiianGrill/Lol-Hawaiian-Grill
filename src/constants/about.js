2f4bb468a9c7b2764e295c9202b8088bff991664
Ashleigh <ashleigh.schuldt@gmail.com>
2018-07-06 15:58:53 -0600

    final touches

--- src/constants/about.js
+++ src/constants/about.js
@@ -1,9 +1,7 @@
 export class Messages {
-    static header = 'ABOUT';
-    static paragraph1 = 'LOL Hawaiian Grill is owned and operated by the Toleafoa (toe-lay-uh-foe-uh) family. Lana, Lopi, Junior, and Juanina (Nina). They have Hawaiian and Samoan roots and are proud of their Polynesian heritge.';
-    static paragraph2 = 'In 2011 The Toleafoa’s moved from Hawaii to Utah and set up shop in American Fork. LOL Island Stylin was a unique store that featured polynesian art, fashion and food. The crowds began to come and the big draw was their truely, authentic polynesian cooking. Stepping into their store was like stepping into an island family kitchen. Good food filled the air and the the stomaches of all who entered.';
-    static paragraph3 = 'In 2015 they completed a total remodel of the store to accomodate for their large following and for special events. LOL had become a social gathering place for all, but espechially for Polynesians wanting a taste of home.';
-    static paragraph4 = 'The Toleafoas were surprised in the middle of 2016 when they were not able to renew their lease. They began a search for a new place to settle. In the meantime they purchased a food truck and continued to serve their strong following in Utah county.';
-    static paragraph5 = 'Near end of 2016 the store was reopened and rebranded in Sandy City as LOL Hawaiian Grill. Their unique brand continues to draw in the crowds, especially Thursday - Saturday (the Poly Plate days).';
-    static footer =  'MAHALO PLENNY';
+    static paragraph1 = 'LOL Hawaiian Grill is owned and operated by the Toleafoa (toe-lay-uh-foe-uh) family, Lana, Lopi, Junior and Juanina. They have Hawaiian and Samoan roots and are proud of their Polynesian heritage.';
+    static paragraph2 = 'In 2011 The Toleafoa’s moved from Hawaii to Utah and set up shop in American Fork. LOL Island Stylin was a unique store that featured polynesian art, music, film, clothing and packaged foods. The crowds really began to flow when they introduced their fresh, home-style cooking. Stepping into their store was like stepping into an island family kitchen. Good food filled the air and the stomaches of all who entered.';
+    static paragraph3 = 'The American Fork LOL became portable in 2015 with the introduction of the LOL Hawaiian Grill food truck. Near the end of 2016 they expanded with the opening of a new restraunt in Sandy, Utah. ';
+    static paragraph4 = 'LOL is all about authenticity and the aloha spirit; it’s unapologetically Polynesian. Once you try their poly plate daily specials you’ll never look for a taste of the islands anywhere else. ';
+    static paragraph5 = '';
 }
